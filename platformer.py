1de65ff09657d5667d21546c32085b2bff14f5aa
ramnik000 <giramnik@sheridancollege.ca>
2024-12-05 14:50:37 -0500

    Loaded images to the screen

--- platformer.py
+++ platformer.py
@@ -3,17 +3,25 @@ from pygame.locals import *
 
 pygame.init()
 
-screen_width = 500
-screen_height = 500
+screen_width = 800
+screen_height = 700
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("PlatformerGame")
 
+bg_image = pygame.image.load('img/sky.png')
+bg_image = pygame.transform.scale(bg_image, (800, 700))
+
 run = True
 while run:
 
+    screen.blit(bg_image,(0,0))
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    #display images
+    pygame.display.update()
 
 pygame.quit()
