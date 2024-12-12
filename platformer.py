7461c05aa3522adb73f8bef6a1826eaeb6fe40d5
ramnik000 <giramnik@sheridancollege.ca>
2024-12-12 13:13:06 -0500

    Created The World for Game

--- platformer.py
+++ platformer.py
@@ -3,20 +3,65 @@ from pygame.locals import *
 
 pygame.init()
 
-screen_width = 800
-screen_height = 700
+screen_width = 1000
+screen_height = 1000
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("PlatformerGame")
 
+#defining game variables
+tile_size = 50
+
 bg_image = pygame.image.load('img/sky.png')
-bg_image = pygame.transform.scale(bg_image, (800, 700))
+
+
+class World():
+    def __init__(self, data):
+        self.tile_list =[]
+        #load images
+        dirt_img = pygame.image.load('img/dirt.png')
+        grass_img = pygame.image.load('img/grass.png')
+
+        row_count =0
+        for row in data:
+            col_count =0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale( dirt_img,(tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    img = pygame.transform.scale( grass_img,(tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                col_count += 1
+            row_count += 1
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+
+world_data = [ 
+[1, 1, 1, 1, 1],
+[1, 0, 0, 0, 1],
+[1, 0, 0, 0, 1],
+[1, 0, 0, 0, 1],
+[1, 2, 2, 2, 1],
+]
+
+world = World(world_data)
 
 run = True
 while run:
 
     screen.blit(bg_image,(0,0))
-    
+
+    world.draw()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
