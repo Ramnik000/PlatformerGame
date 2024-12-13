1ba71d79f05fc07a236f61e9d60d0134e45cb5e2
ramnik000 <giramnik@sheridancollege.ca>
2024-12-13 16:33:46 -0500

    Added Control to the Player

--- platformer.py
+++ platformer.py
@@ -21,8 +21,40 @@ class Player():
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.vel_y = 0 #jump
+        self.jumped = False
     
     def update(self):
+        dx = 0
+        dy = 0
+        #get keypresses
+        key = pygame.key.get_pressed()
+        if key[pygame.K_SPACE] and self.jumped == False:
+            self.vel_y = -15
+            self.jumped = True
+        if key[pygame.K_SPACE] == False:
+            self.jumped = False
+        if key[pygame.K_LEFT]:
+            dx -= 5
+        if key[pygame.K_RIGHT]:
+            dx += 5
+
+        #add gravity
+        self.vel_y += 1
+        if self.vel_y > 10:
+            self.vel_y = 10
+        dy += self.vel_y
+        #Check the collisons 
+    
+        #Update the player coordinates
+        self.rect.x += dx
+        self.rect.y += dy
+
+        if self.rect.bottom > screen_height:
+            self.rect.bottom = screen_height
+            dy =0
+
+
         # draw the player on the screen
         screen.blit(self.image, self.rect)
         
